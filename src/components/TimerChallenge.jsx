174881e60fef5039ba32f425e1038b8b1e784b61
jeffry.miranda <jeffry.miranda@daugherty.cr>
2024-11-29 16:31:55 -0600

    Adding a Modal Component

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,4 +1,5 @@
 import {useRef, useState} from "react";
+import ResultModal from "./ResultModal.jsx";
 
 export default function ({title, targetTime}) {
     const timer = useRef();
@@ -17,20 +18,22 @@ export default function ({title, targetTime}) {
     }
 
     return (
-        <section className="challenge">
-            <h2>{title}</h2>
-            {timerExpired && <p>You lost!</p>}
-            <p className="challenge-time">
-                {targetTime} second{targetTime > 1 ? 's' : ''}
-            </p>
-            <p>
-                <button onClick={timerStarted ? handleStop : handleStart}>
-                    {timerStarted ? 'Stop' : 'Start'} Challenge
-                </button>
-            </p>
-            <p className="">
-                {timerStarted ? 'Time is running' : 'Time inactive'}
-            </p>
-        </section>
+        <>
+            {timerExpired && <ResultModal targetTime={targetTime} result="lost"/>}
+            <section className="challenge">
+                <h2>{title}</h2>
+                <p className="challenge-time">
+                    {targetTime} second{targetTime > 1 ? 's' : ''}
+                </p>
+                <p>
+                    <button onClick={timerStarted ? handleStop : handleStart}>
+                        {timerStarted ? 'Stop' : 'Start'} Challenge
+                    </button>
+                </p>
+                <p className="">
+                    {timerStarted ? 'Time is running' : 'Time inactive'}
+                </p>
+            </section>
+        </>
     );
 }
